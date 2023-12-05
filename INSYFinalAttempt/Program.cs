4a90d8a7c9841083bdfd02d1d5bae35c88229a71
Nicholas Dembinsky <nicholas.dembinsky@marquette.edu>
2023-12-04 18:15:58 -0600

    Add project files.

---
+++ INSYFinalAttempt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace INSYFinalAttempt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
